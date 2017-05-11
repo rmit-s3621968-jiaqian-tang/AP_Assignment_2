e3ccbdfcc6b9f342cb38237fd69ab93489612496
Antoine_0x80 <rmit-s3621968-jiaqian-tang@users.noreply.github.com>
2017-05-11 17:51:15 +1000

    Add files via upload

---
+++ assignment2/code/src/com.ap.ozlympic.controller/ViewRN.java
@@ -0,0 +1,39 @@
+
+package com.ap.ass2.controller;
+
+import javafx.collections.ObservableList;
+import javafx.fxml.Initializable;
+import java.net.URL;
+import java.sql.*;
+import java.util.ResourceBundle;
+
+import com.ap.ass2.model.Message;
+
+import javafx.collections.FXCollections;
+import com.ap.ass2.model.SQLHelper;
+
+public class ViewRN implements Initializable {
+
+	public static ObservableList<Message> getList() {
+		ObservableList<Message> list = FXCollections.observableArrayList();
+	SQLHelper sp=new SQLHelper();
+	try{
+	String sql="select * from Participant where Type like 'sprinter' or  Type like 'super';";
+	ResultSet rs=sp.query(sql);
+		
+			while (rs.next()) {
+				list.add(new Message(rs.getString("ID"), rs.getString("Type"), rs.getString("Name"), rs.getInt("Age"),
+						rs.getString("State")));
+			}
+		} catch (Exception e2) {
+			System.err.println(e2);
+		}
+		return list;
+	}
+
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		// TODO Auto-generated method stub
+
+	}
+}
