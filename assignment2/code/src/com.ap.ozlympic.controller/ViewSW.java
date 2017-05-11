07ce19cd0951aa2e4ad17aaa25bf0f6433ff8514
Antoine_0x80 <rmit-s3621968-jiaqian-tang@users.noreply.github.com>
2017-05-11 17:53:55 +1000

    Update ViewSW.java

--- assignment2/code/src/com.ap.ozlympic.controller/ViewSW.java
+++ assignment2/code/src/com.ap.ozlympic.controller/ViewSW.java
@@ -1,5 +1,5 @@
 
-package com.ap.ass2.controller;
+package com.ap.ozlympic.controller;
 
 import javafx.collections.ObservableList;
 import javafx.fxml.Initializable;
@@ -7,10 +7,10 @@ import java.net.URL;
 import java.sql.*;
 import java.util.ResourceBundle;
 
-import com.ap.ass2.model.Message;
+import com.ap.ozlympic.model.Message;
 
 import javafx.collections.FXCollections;
-import com.ap.ass2.model.SQLHelper;
+import com.ap.ozlympic.model.SQLHelper;
 
 public class ViewSW implements Initializable {
 
