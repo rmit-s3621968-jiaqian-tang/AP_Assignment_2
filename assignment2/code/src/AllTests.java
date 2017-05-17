35ea32aaf02f9a5bdb39f6e4dcc4500d2569087d
Katrine Ren <s3617315@student.rmit.edu.au>
2017-05-17 23:56:35 +1000

    Update AllTests.java

--- assignment2/code/src/AllTests.java
+++ assignment2/code/src/AllTests.java
@@ -5,7 +5,7 @@ import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
-@SuiteClasses({ SprinterTest.class, SQLHelperTest.class })
+@SuiteClasses({ SprinterTest.class, SQLHelperTest.class,OfficialTest.class  })
 public class AllTests {
 	
 	
