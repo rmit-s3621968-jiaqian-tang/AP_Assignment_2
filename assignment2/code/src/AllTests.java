1464bd193bdc217ef7a5d7362c2464fd7d9dee30
Katrine Ren <s3617315@student.rmit.edu.au>
2017-05-17 19:44:47 +1000

    Add files via upload

---
+++ assignment2/code/src/AllTests.java
@@ -0,0 +1,13 @@
+package UnitTest;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ SprinterTest.class, SQLHelperTest.class })
+public class AllTests {
+	
+	
+
+}
